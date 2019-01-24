71e5d80b36604dba3385eb295c91f3bc4ab4d36b
Nnavi <nnavi@students.softuni.bg>
2019-01-24 14:16:30 +0200

    1. Ages

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = int.Parse(Console.ReadLine());
+            if (age>=0 && age<=2)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (age>2 && age<=13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (age>13 && age<=19)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (age>19 && age<=65)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (age>65)
+            {
+                Console.WriteLine("elder");
+            }
         }
     }
 }
